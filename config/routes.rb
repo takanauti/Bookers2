868891503323a6d2c52e27678885223cd05a1755
takanauti <git@github.com:takanauti/git-practice.git>
2022-12-31 07:27:31 +0000

    bookers2

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'top' => 'homes#top'
   root to: 'homes#top'
-  resources :books
+  devise_for :users
+  resources :books, only: [:new, :create, :index, :show, :destroy]
+  get "homes/about" => "homes#about", as: "about"
+
 end
